cebd26333d088105a7ad8d1cd586fcfc64473caf
Jordan Fisher <jordan@pwnee.com>
2012-12-07 19:23:44 -0500

    Added renamed files.

---
+++ Game/Game/Player/Hero Physics/BobPhsxRandom.h
@@ -0,0 +1,29 @@
+﻿#ifndef BOB_PHSX_RANDOM_H
+#define BOB_PHSX_RANDOM_H
+
+#include <global_header.h>
+
+namespace CloudberryKingdom
+{
+	class BobPhsxRandom : public BobPhsx
+	{
+		// Singleton
+	protected:
+		virtual void InitSingleton();
+	private:
+		static const std::shared_ptr<BobPhsxRandom> instance;
+	public:
+		static const std::shared_ptr<BobPhsxRandom> &getInstance();
+
+		static std::shared_ptr<BobPhsx> ChooseHeroType();
+
+		virtual std::shared_ptr<BobPhsx> Clone();
+		void CopyTo( const std::shared_ptr<BobPhsxRandom> &bob );
+
+		// Instancable class
+		BobPhsxRandom();
+	};
+}
+
+
+#endif	//#ifndef RANDOM
