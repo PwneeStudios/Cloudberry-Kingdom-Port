a6b9691ba998ed59223d63522aba73e74f66615e
Oleg Kozhushnyan <oleg.kozhushnyan@gmail.com>
2012-12-13 17:47:42 -0500

    Forgot a file

---
+++ Game/Hacks/XNA/GamePad.cpp
@@ -0,0 +1,12 @@
+#include "GamePad.h"
+
+#include <enums.h>
+
+GamePadState GamePad::GetState( PlayerIndex index )
+{
+	return GamePadState();
+}
+
+void GamePad::SetVibration( PlayerIndex index, float left, float right )
+{
+}
