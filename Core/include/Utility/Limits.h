38f90362c517b5524331f2779f74682155fe4181
Jordan Fisher <jordan@pwnee.com>
2012-12-16 21:53:11 -0500

    Fixed compiler errors for WiiU.

--- Core/include/Utility/Limits.h
+++ Core/include/Utility/Limits.h
@@ -9,4 +9,12 @@
 #define __min( a, b )  ( ( ( a ) < ( b ) ) ? ( a ) : ( b ) )
 #endif
 
+#ifndef FLT_MIN
+#define FLT_MIN         1.175494351e-38F        /* min positive value */
+#endif
+
+#ifndef FLT_MAX
+#define FLT_MAX         3.402823466e+38F        /* max value */
+#endif
+
 #endif
