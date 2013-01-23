d41669c82c26885cdaa91d85daf22af84b615602
Oleg Kozhushnyan <oleg.kozhushnyan@gmail.com>
2013-01-22 19:33:35 -0500

    Music stream!

--- Core/src/Audio/MediaPlayerPS3.cpp
+++ Core/src/Audio/MediaPlayerPS3.cpp
@@ -3,6 +3,8 @@
 #include <Utility/Log.h>
 
 #include <Audio/PS3/mscommon.h>
+#include <Audio/Song.h>
+#include "SongInternalPS3.h"
 
 static int SUBNUM = 1;
 static int PLAYSUB = SUBNUM | CELL_MS_BUS_FLAG;
@@ -13,6 +15,10 @@ static long fps60 = 1000000 / 60;
 static void *mp3Memory = NULL;
 
 static void MultiStreamUpdateThread( uint64_t param );
+
+void *songBuffer = NULL;
+long stream = -1;
+float musicVolume;
 
 void MediaPlayer::Initialize()
 {
@@ -53,6 +59,7 @@ void MediaPlayer::Initialize()
 	StartMultiStreamUpdateThread( MultiStreamUpdateThread );
 	multiStreamUpdateThreadRunning = true;
 
+	songBuffer = memalign( 128, 10 * 1024 * 1024 );
 }
 
 void MediaPlayer::Shutdown()
@@ -62,38 +69,177 @@ void MediaPlayer::Shutdown()
 
 	delete[] mp3Memory;
 	mp3Memory = NULL;
+
+	free( songBuffer );
+}
+
+int LoadMP3(const char *filename, long *addr, long *size)
+{
+	unsigned int tSize=0;	// total size
+	float tTime=0;			// total time
+	int ret;
+	CellMSMP3FrameHeader Hdr;
+	unsigned int offset=0;
+
+	if(!(filename && addr && size))
+		return -1;
+
+	long pData;
+
+    // Open PCM sample data
+    int nFileHandle=OpenFile( filename, size);
+
+	assert( *size < 10 * 1024 * 1024 );
+
+    if(nFileHandle<0)
+    {
+        printf(" ERROR opening file - %s, exiting.\n", filename);
+        while(1){};
+    }
+
+    // Allocate memory for a sample buffer
+	pData = (long)songBuffer;
+	// Load file.
+	LoadFile(nFileHandle,pData,*size,0,0);
+
+	*addr = pData;
+
+	while(1)
+	{
+		ret=cellMSMP3GetFrameInfo((void*)pData,&Hdr);
+		if (ret==-1)
+		{
+			printf("Invalid MP3 header\n");
+			printf("Offset=0x%x\n",offset);
+			return (-1);	// Invalid MP3 header
+		}
+
+		tSize+=Hdr.PacketSize;	// Update total file size
+		if ((Hdr.ID3==0)&&(Hdr.Tag==0))
+			tTime+=Hdr.PacketTime;	// Update total playing time (in seconds)
+		pData+=Hdr.PacketSize;	// Move forward to next packet
+		offset+=Hdr.PacketSize;
+
+		if (tSize==(unsigned int)*size)
+		{
+			printf("MP3 File is valid.\n");
+			printf("Total Playback Time at %d Hz = (secs): %f\n",Hdr.Frequency,tTime);
+			return(nFileHandle);
+		}
+		else if (tSize>(unsigned int)*size)
+		{
+			printf("ERROR: PASSED END OF FILE!\n");
+			printf("%x,%x\n",tSize,(int)*size);
+			while(1){};
+		}
+	}
+}
+
+long TriggerStream( long nCh,long pSampleData1, long pSampleData2, const long nSize, const long nSize2, const long nFrequency, const long nSampleChannels )
+{
+CellMSInfo  MS_Info;
+float vol;
+
+	//suppress compiler warnings
+	SUPPRESS_COMPILER_WARNING(nSampleChannels);
+
+
+    MS_Info.SubBusGroup         = CELL_MS_MASTER_BUS;
+
+// Set address and size of data to play
+
+    MS_Info.FirstBuffer         = (void *)((long)pSampleData1);
+    MS_Info.FirstBufferSize     = nSize; // size in bytes
+    MS_Info.SecondBuffer         = (void *)((long)pSampleData2);
+    MS_Info.SecondBufferSize     = nSize2; // size in bytes
+
+	// Set pitch and number of channels
+    MS_Info.Pitch               = nFrequency;
+    MS_Info.numChannels         = 2;
+	MS_Info.flags				= CELL_MS_STREAM_AUTOCLOSE;
+
+	// Initial delay (in samples) before playback starts. Allows for sample accurate playback
+	MS_Info.initialOffset		= 0;
+
+	// Input data type
+	MS_Info.inputType = CELL_MS_MP3;
+
+    cellMSStreamSetInfo(nCh, &MS_Info);
+
+// Note: No callback has been setup. This sound will just constantly play. No callbacks required.
+
+    cellMSStreamPlay(nCh);
+
+	cellMSCoreSetVolume1(nCh, CELL_MS_DRY, CELL_MS_SPEAKER_FL, CELL_MS_CHANNEL_0, musicVolume);
+	cellMSCoreSetVolume1(nCh, CELL_MS_DRY, CELL_MS_SPEAKER_FR, CELL_MS_CHANNEL_0, musicVolume);
+    return nCh;
 }
 
 void MediaPlayer::Play( const boost::shared_ptr<Song> &song )
 {
+	if( stream >= 0 )
+	{
+		cellMSStreamClose( stream );
+		stream = -1;
+	}
+
+	long addr, size;
+	LoadMP3( song->internal_->Path.c_str(), &addr, &size );
+
+	stream = cellMSStreamOpen();
+	sys_timer_usleep( fps60 * 60 * 2 );
+	stream = TriggerStream( stream, addr, addr, size, size, 44100, 2 );
 }
 
 void MediaPlayer::Pause()
 {
+	if( stream < 0 )
+		return;
+
+	cellMSCoreSetPause( stream, CELL_MS_PAUSED );
 }
 
 void MediaPlayer::Resume()
 {
+	if( stream < 0 )
+		return;
+
+	cellMSCoreSetPause( stream, CELL_MS_NOTPAUSED );
 }
 
 void MediaPlayer::Stop()
 {
+	if( stream < 0 )
+		return;
+
+	cellMSStreamClose( stream );
+	stream = -1;
 }
 
 MediaState MediaPlayer::GetState()
 {
-	return MediaState_Paused;
+	if( stream < 0 )
+		return MediaState_Paused;
+
+	return cellMSCoreGetPause( stream ) == CELL_MS_PAUSED ? MediaState_Paused : MediaState_Playing;
 }
 
 bool MediaPlayer::IsRepeating = false;
 
 void MediaPlayer::SetVolume( float volume )
 {
+	musicVolume = volume;
+
+	if( stream >= 0 )
+	{
+		cellMSCoreSetVolume1(stream, CELL_MS_DRY, CELL_MS_SPEAKER_FL, CELL_MS_CHANNEL_0, musicVolume);
+		cellMSCoreSetVolume1(stream, CELL_MS_DRY, CELL_MS_SPEAKER_FR, CELL_MS_CHANNEL_0, musicVolume);
+	}
 }
 
 float MediaPlayer::GetVolume()
 {
-	return 0.f;
+	return musicVolume;
 }
 
 void MultiStreamUpdateThread( uint64_t param )
--- Core/src/Audio/SongPS3.cpp
+++ Core/src/Audio/SongPS3.cpp
@@ -1,13 +1,76 @@
 #include <Audio/Song.h>
 
+#include <Audio/PS3/mscommon.h>
 #include <Content/File.h>
 #include <Content/Filesystem.h>
+#include <Utility/Log.h>
 
 #include "SongInternalPS3.h"
+
+float GetMP3Length( const std::string &path )
+{
+	long size;
+
+	int handle = OpenFile( path.c_str(), &size );
+
+	if( handle < 0 )
+	{
+		LOG.Write( "Couldn't get length: %s\n", path.c_str() );
+		return 0.f;
+	}
+
+	LOG.Write( "Buffer %d bytes\n", size );
+	long buffer = reinterpret_cast< long >( memalign( 128, size ) );
+	long data = buffer;
+
+	LoadFile( handle, data, size, 0, 0 );
+
+	unsigned int tSize = 0;
+	float time = 0.f;
+	unsigned int offset = 0;
+
+	while( true )
+	{
+		CellMSMP3FrameHeader hdr;
+
+		int ret = cellMSMP3GetFrameInfo( reinterpret_cast< void * >( data ), &hdr );
+		if( ret == -1 )
+		{
+			LOG.Write( "Invalid MP3 header\n" );
+			time = 0.f;
+			break;
+		}
+
+		tSize += hdr.PacketSize;
+		if( hdr.ID3 == 0 && hdr.Tag == 0 )
+			time += hdr.PacketTime;
+		data += hdr.PacketSize;
+		offset += hdr.PacketSize;
+		
+		if( tSize == size )
+		{
+			LOG.Write( "MP3 loaded\n" );
+			LOG.Write( "Playback time at %d Hz: %f s\n", hdr.Frequency, time );
+			break;
+		}
+		else if( tSize > size )
+		{
+			LOG.Write( "Passed the end of file!\n ");
+			time = 0.f;
+			break;
+		}
+	}
+
+	free( reinterpret_cast< void * >( buffer ) );
+	cellFsClose( handle );
+
+	return time;
+}
 
 Song::Song() :
 	internal_( new SongInternal )
 {
+	Duration.TotalSeconds = 0.f;
 }
 
 Song::~Song() 
@@ -17,4 +80,6 @@ Song::~Song()
 
 void Song::Load( const std::string &path )
 {
+	internal_->Path = "/app_home/" + path;
+	Duration.TotalSeconds = GetMP3Length( internal_->Path );
 }
