5df860ec9ccf9670c668a5a28628d3cef1b4b994
Oleg Kozhushnyan <oleg.kozhushnyan@gmail.com>
2013-03-06 02:51:49 -0500

    Comment oops

--- Core/src/Input/GamePadWiiU.cpp
+++ Core/src/Input/GamePadWiiU.cpp
@@ -16,7 +16,7 @@ bool anythingElseConnected;
 /*
 KPADStatus kpadStatus[ WPAD_MAX_CONTROLLERS ];
 s32 kpadReadLength[ WPAD_MAX_CONTROLLERS ];
-/*
+*/
 static void ConnectCallback( s32 chan, s32 reason )
 {
 	if( reason >= 0 )
