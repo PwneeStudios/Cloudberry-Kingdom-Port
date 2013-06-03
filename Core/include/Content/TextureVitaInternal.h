6ce40b3d72127f230efbf9371041e214e45d5077
Jordan Fisher <jordan@pwnee.com>
2013-06-03 16:26:37 -0400

    Added extra files that were hiding in another content folder.

---
+++ Core/include/Content/TextureVitaInternal.h
@@ -0,0 +1,11 @@
+#ifndef _TEXTURE_VITA_INTERNAL_H_
+#define _TEXTURE_VITA_INTERNAL_H_
+
+struct TextureVitaInternal
+{
+	//psglTextureReference Ref;
+	unsigned char *FileData;
+	unsigned int FileLength;
+};
+
+#endif
